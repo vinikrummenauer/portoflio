aca3921b5b9d1fadf35e9d86e9ecd8e65e694548
Vinicius Krummenauer <viniciuskdepaula@gmail.com>
2023-09-09 17:24:10 -0300

    :construction: feat:2 att portfolio

--- components/contact_me.tsx
+++ components/contact_me.tsx
@@ -4,9 +4,6 @@ import React from "react";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
-import { sendEmail } from "@/actions/sendEmail";
-import SubmitBtn from "./submit-btn";
-import toast from "react-hot-toast";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
